ccc36140656448ec1234a8b43e4f2d9adfbb1ca0
asalcedo31 <adrisal31@gmail.com>
2018-02-09 17:26:52 -0500

    added test cases for move ants

--- test_time_step.cc
+++ test_time_step.cc
@@ -35,23 +35,34 @@ struct myFixture{
 //BOOST_GLOBAL_FIXTURE(myFixture);
 BOOST_FIXTURE_TEST_SUITE(countAntsSuite, myFixture)
 
-BOOST_AUTO_TEST_CASE(antCountsRight)
+BOOST_AUTO_TEST_CASE(antCountsRight) // check if the count of ants matches the total after initialization
 {
 	int ant_count = count_ants(table_size, number_of_ants);
 	std::cout << ant_count << std::endl;
-	BOOST_CHECK(count_ants(table_size, number_of_ants) ==total_ants); //check that the total number of ants counted is positive
+	BOOST_CHECK(count_ants(table_size, number_of_ants) == total_ants); //check that the total number of ants counted is positive
 }
 
-// BOOST_AUTO_TEST_CASE(moveAntsNotAdding)
-// {
-// 	int **number_of_ants = init_number_of_ants(10);
-// 	int **new_number_of_ants = init_number_of_ants(10);
-// 	float **crawl_direction = init_crawl_direction(table_size);
-// 	zero_array(10,number_of_ants); //initialize small test example array
-// 	place_ants_t0(10,number_of_ants,5);
-// 	int ant_count = count_ants(10, number_of_ants);
-// 	zero_array(10,new_number_of_ants); // zero new number of ants array
-//     move_ants(10, crawl_direction, new_number_of_ants, number_of_ants); // move ants according to the crawl direction at that coordinate
-// }
+BOOST_AUTO_TEST_CASE(moveAntsNotAdding)
+{
+	move_ants(table_size, crawl_direction, new_number_of_ants, number_of_ants); // move ants according to the crawl direction at that coordinate
+	int new_ant_count = count_ants(table_size, new_number_of_ants); // count the ants in the repositioned array
+	BOOST_CHECK(new_ant_count <= total_ants); // check to make sure the new total is less than or equal to the total we started with
+}
+
+BOOST_AUTO_TEST_CASE(moveAntsNotMoving)
+{
+	move_ants(table_size, crawl_direction, new_number_of_ants, number_of_ants); // move ants according to the crawl direction at that coordinate
+	bool diff = false; // assume the arrays are identical
+	int i = 0;
+    while (i<table_size && diff == false) {  //iterates through table coordinates until it finds a cell where the values are different
+        for (int j=0;j<table_size;j++) {
+        	if(new_number_of_ants[i][j] != number_of_ants[i][j]){ // check the value of a given position in the old and new array
+        		diff = true; // check if different 
+        	}
+        }
+        i++;
+    }
+	BOOST_CHECK(diff == true); // make sure the arrays have at least one value that's different 
+}
 
 BOOST_AUTO_TEST_SUITE_END( )
