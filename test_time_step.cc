adfb7af85b2b446d646bbac19aefa7c09929f6c3
asalcedo31 <adrisal31@gmail.com>
2018-02-09 17:47:26 -0500

    moved identical array check to its own function

--- test_time_step.cc
+++ test_time_step.cc
@@ -6,6 +6,20 @@
 #define BOOST_TEST_MODULE TestTimeStep
 #include <boost/test/included/unit_test.hpp>
 
+// little function to check if two arrays of the same size are identical
+bool check_identical(int **new_number_of_ants, int **number_of_ants, int table_size){
+	bool diff = false; // assume the arrays are identical
+	int i = 0;
+    while (i<table_size && diff == false) {  //iterates through table coordinates until it finds a cell where the values are different
+        for (int j=0;j<table_size;j++) {
+        	if(new_number_of_ants[i][j] != number_of_ants[i][j]){ // check the value of a given position in the old and new array
+        		diff = true; // check if different 
+        	}
+        }
+        i++;
+    }
+    return diff;
+}
 // set up the objects the test functions will need 
 struct myFixture{
 	//declare variables
@@ -23,7 +37,7 @@ struct myFixture{
 		place_ants_t0(table_size,number_of_ants,total_ants);
 		new_number_of_ants = init_number_of_ants(table_size); //initialize small test example new ants array
 		zero_array(table_size,new_number_of_ants); //zero it
-		crawl_direction = init_crawl_direction(table_size);
+		crawl_direction = init_crawl_direction(table_size); // initialize crawl direction 
 	} //clean up 
 	~myFixture(){
 		deallocate_int_matrix(number_of_ants);
@@ -32,13 +46,13 @@ struct myFixture{
 	}
 	
 };
-//BOOST_GLOBAL_FIXTURE(myFixture);
+
+// Use a fixture test suite so 
 BOOST_FIXTURE_TEST_SUITE(countAntsSuite, myFixture)
 
 BOOST_AUTO_TEST_CASE(antCountsRight) // check if the count of ants matches the total after initialization
 {
 	int ant_count = count_ants(table_size, number_of_ants);
-	std::cout << ant_count << std::endl;
 	BOOST_CHECK(count_ants(table_size, number_of_ants) == total_ants); //check that the total number of ants counted is positive
 }
 
@@ -52,16 +66,7 @@ BOOST_AUTO_TEST_CASE(moveAntsNotAdding)
 BOOST_AUTO_TEST_CASE(moveAntsNotMoving)
 {
 	move_ants(table_size, crawl_direction, new_number_of_ants, number_of_ants); // move ants according to the crawl direction at that coordinate
-	bool diff = false; // assume the arrays are identical
-	int i = 0;
-    while (i<table_size && diff == false) {  //iterates through table coordinates until it finds a cell where the values are different
-        for (int j=0;j<table_size;j++) {
-        	if(new_number_of_ants[i][j] != number_of_ants[i][j]){ // check the value of a given position in the old and new array
-        		diff = true; // check if different 
-        	}
-        }
-        i++;
-    }
+	bool diff = check_identical(new_number_of_ants, number_of_ants, table_size);
 	BOOST_CHECK(diff == true); // make sure the arrays have at least one value that's different 
 }
 
@@ -69,17 +74,8 @@ BOOST_AUTO_TEST_CASE(updateRight)
 {
 	move_ants(table_size, crawl_direction, new_number_of_ants, number_of_ants); // move ants according to the crawl direction at that coordinate
 	update_ant_arrays(table_size, new_number_of_ants, number_of_ants); // update the arrays
-	bool diff = false; // assume the arrays are identical
-	int i = 0;
-    while (i<table_size && diff == false) {  //iterates through table coordinates until it finds a cell where the values are different
-        for (int j=0;j<table_size;j++) {
-        	if(new_number_of_ants[i][j] != number_of_ants[i][j]){ // check the value of a given position in the old and new array
-        		diff = true; // check if different 
-        	}
-        }
-        i++;
-    }
-	BOOST_CHECK(diff == false); // make sure the old and new arrays are identical
+	bool diff = check_identical(new_number_of_ants, number_of_ants, table_size); 
+	BOOST_CHECK(diff == false); // make sure the updated and new arrays are identical
 }
 
 
